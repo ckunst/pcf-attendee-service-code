cf469c5bd9b1783f12ac448a3ff36e4205c9d0bf
Cristian Kunst <ckunst@pivotal.io>
2017-03-22 09:57:26 +0100

    Test commit

--- src/main/java/io/pivotal/AttendeeServiceApplication.java
+++ src/main/java/io/pivotal/AttendeeServiceApplication.java
@@ -5,7 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class AttendeeServiceApplication {
-	
+	//A new commit
 	public static void main(String[] args) {
 		SpringApplication.run(AttendeeServiceApplication.class, args);
 	}
